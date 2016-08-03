022f92bb602679b7280bdee14eefc6d85f4c2cf5
Key Hoffman <keyhoffman@keys-air>
2016-08-03 00:45:53 -0500

    initial commit

--- Five/SeperatorView.swift
+++ Five/SeperatorView.swift
@@ -16,6 +16,6 @@ final class SeperatorView: UIView {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has nt been implemented")
     }
 }
